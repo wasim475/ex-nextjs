e3d7c962faebb4fca50cf52df18ce09dd39ee09e
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-23 08:27:03 +0600

    added note layout and page folder

--- app/(registry)/students/[slug]/page.js
+++ app/(registry)/students/[slug]/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const page = () => {
+const page = ({params}) => {
+    console.log(params.slug)
   return (
     <div>
-      <h1>ami holam slag.</h1>
+      <h1>ami holam {params.slug}</h1>
     </div>
   )
 }
--- app/(registry)/students/page.js
+++ app/(registry)/students/page.js
@@ -1,10 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 
-const page = () => {
+const getData = async ()=>{
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  if(!res.ok){
+   return "data not found"
+    
+  }
+  return res.json()
+}
+const page = async () => {
+  const data = await getData()
+  console.log(data)
   return (
     <div>
       <ul>
-        <li>student-1</li>
+        <li>
+          <Link href={"/students/nafi"} >student-1</Link>
+        </li>
         <li>student-2</li>
         <li>student-3</li>
       </ul>
