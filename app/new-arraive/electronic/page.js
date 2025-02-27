6408e0b1bf22e9fc2e5942704c1028af9e9e694c
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-27 07:54:50 +0600

    Update page.js

--- app/new-arraive/electronic/page.js
+++ app/new-arraive/electronic/page.js
@@ -7,7 +7,7 @@ export default function Electric(){
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt eos, vel aperiam quaerat, illo reprehenderit dolorem soluta in dolore illum incidunt laborum est. Illo, quas? Temporibus, officiis! Tempora deleniti odio ex necessitatibus reiciendis quas et assumenda, dolores natus. Perferendis tenetur illo blanditiis fuga officiis doloribus, voluptas mollitia veniam enim necessitatibus quidem minima possimus nostrum numquam. Suscipit recusandae excepturi, aspernatur nulla corrupti impedit a veniam nisi ullam doloribus adipisci dolorum ipsam explicabo officiis libero natus qui, rerum provident eum aliquid aut sint quibusdam. Ullam reiciendis quasi fugiat totam animi dignissimos molestias modi asperiores, maiores sequi. Ratione qui cupiditate repellendus hic placeat.
             </p>
-            <Button prefetch={true} path={'/new-arraive/education'} >go to education</Button>
+            <Button prefetch={false} path={'/new-arraive/education'} >go to education</Button>
         </div>
     );
 }
