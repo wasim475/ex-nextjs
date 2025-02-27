c214c37ec3395cc8e2d6ee32dc258fc9df90b62b
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-27 13:57:10 +0600

    Update global-error.js

--- app/global-error.js
+++ app/global-error.js
@@ -1,24 +1,28 @@
-'use client' // Error components must be Client Components
- 
-import { useEffect } from 'react'
- 
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+
 export default function Error({ error, reset }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
+    console.error(error);
+  }, [error]);
+
   return (
-    <div>
-      <h2>Something went wrong in rootlayout!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  )
+    <html>
+      <body>
+        <div>
+          <h2>Something went wrong in rootlayout!</h2>
+          <button
+            onClick={
+              // Attempt to recover by trying to re-render the segment
+              () => reset()
+            }
+          >
+            Try again
+          </button>
+        </div>
+      </body>
+    </html>
+  );
 }
