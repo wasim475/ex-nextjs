4b1ca48bb8ea0270ccfbfb92fc6ddbf5e0db1f85
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-27 07:47:51 +0600

    added a practice folder

--- app/page.js
+++ app/page.js
@@ -1,19 +1,11 @@
+import SortProducts from './practpage/sortProducts';
 
 
 export default function Home() {
   // const [counter, setCounter]= useState()
   return (
     <main>
-      <div>
-       <ul className="flex gap-x-5">
-        <li >nav-1</li>
-        <li>nav-2</li>
-        <li>nav-3</li>
-        <li>nav-4</li>
-        <li>nav-5</li>
-        <li>nav-6</li>
-       </ul>
-      </div>
+     <SortProducts/>
     </main>
   );
 }
