638264172298f97f7ceb808c680ed771328f0bfa
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-16 09:15:08 +0600

    Update page.js

---
+++ app/paralal-dashboard/@quiz/page.js
@@ -0,0 +1,7 @@
+export default function QuizPage() {
+    return (
+        <div className="text-xl p-4 border border-gray-200 rounded h-[360px] flex items-center justify-center">
+            QUIZ
+        </div>
+    );
+}
