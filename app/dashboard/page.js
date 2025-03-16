29663ba937331131afa18017299df4c8b50cb7f5
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-16 08:45:48 +0600

    Update page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,14 @@
+import Assignments from '../components/Assignments';
+import Notifications from '../components/Notifications';
+import Quiz from '../components/Quiz';
+
+
+export default function DashboardPage() {
+    return (
+        <>
+            <Assignments />
+            <Notifications />
+            <Quiz />
+        </>
+    );
+}
