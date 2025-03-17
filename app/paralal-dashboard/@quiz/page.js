6855f476126c105c817f6544ac4ee50883aff8d0
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-17 08:36:47 +0600

    Update page.js

--- app/paralal-dashboard/@quiz/page.js
+++ app/paralal-dashboard/@quiz/page.js
@@ -1,4 +1,7 @@
-export default function QuizPage() {
+import { wait } from '../../lib/wait';
+
+export default async function QuizPage() {
+    await wait(1000)
     return (
         <div className="text-xl p-4 border border-gray-200 rounded h-[360px] flex items-center justify-center">
             QUIZ
