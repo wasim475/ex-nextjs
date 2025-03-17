4180f4fbca17ef1bef9c2b1a4acddb066a7b1672
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-17 08:35:24 +0600

    Update page.js

--- app/paralal-dashboard/@notifications/page.js
+++ app/paralal-dashboard/@notifications/page.js
@@ -1,4 +1,7 @@
-export default function NotificationsPage() {
+import { wait } from '../../lib/wait';
+
+export default async function NotificationsPage() {
+    await wait(2000)
     return (
         <div className="text-xl p-4 row-span-2 border border-gray-200 rounded h-[745px] flex items-center justify-center">
             NOTIFICATIONS
