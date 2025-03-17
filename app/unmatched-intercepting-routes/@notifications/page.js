e55c26811cfb38b5d69feb6d83a0c7421ca085ca
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-17 09:36:57 +0600

    Update page.js

--- app/unmatched-intercepting-routes/@notifications/page.js
+++ app/unmatched-intercepting-routes/@notifications/page.js
@@ -1,10 +1,14 @@
-import { wait } from '../../lib/wait';
+import Link from "next/link";
 
 export default async function NotificationsPage() {
-    await wait(2000)
     return (
-        <div className="text-xl p-4 row-span-2 border border-gray-200 rounded h-[745px] flex items-center justify-center">
-            NOTIFICATIONS
+        <div className="text-xl p-4 row-span-2 border border-gray-200 rounded h-[745px] flex gap-5 items-center justify-center">
+            <span>ALL NOTIFICATIONS</span>
+            <div>
+                <Link href="/parallel-dashboard/seen" className="text-blue-400">
+                    SEEN
+                </Link>
+            </div>
         </div>
     );
 }
