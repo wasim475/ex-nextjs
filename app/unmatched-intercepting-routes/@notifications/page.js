4fd7d4f57d4e401817f9fb21d67326ed0ea64a43
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-17 10:51:05 +0600

    update route

--- app/unmatched-intercepting-routes/@notifications/page.js
+++ app/unmatched-intercepting-routes/@notifications/page.js
@@ -5,7 +5,7 @@ export default async function NotificationsPage() {
         <div className="text-xl p-4 row-span-2 border border-gray-200 rounded h-[745px] flex gap-5 items-center justify-center">
             <span>ALL NOTIFICATIONS</span>
             <div>
-                <Link href="/parallel-dashboard/seen" className="text-blue-400">
+                <Link href="/unmatched-intercepting-routes/seen" className="text-blue-400">
                     SEEN
                 </Link>
             </div>
--- app/unmatched-intercepting-routes/@notifications/seen/page.js
+++ app/unmatched-intercepting-routes/@notifications/seen/page.js
@@ -5,7 +5,7 @@ export default async function SeenNotificationsPage() {
         <div className="text-xl p-4 row-span-2 border border-gray-200 rounded h-[745px] flex gap-5 items-center justify-center">
             <span>SEEN NOTIFICATIONS</span>
             <div>
-                <Link href="/parallel-dashboard" className="text-blue-400">
+                <Link href="/unmatched-intercepting-routes" className="text-blue-400">
                     ALL
                 </Link>
             </div>
