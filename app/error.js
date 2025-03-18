ede29f86a55f9887706146cc0029316e2406291f
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-19 04:38:17 +0600

    Update error.js

--- app/error.js
+++ app/error.js
@@ -10,7 +10,7 @@ export default function Error({ error, reset }) {
  
   return (
     <div>
-      <h2>Something went wrong under rootlayout!</h2>
+      <h2>Something went wrong under Rootlayout!</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
