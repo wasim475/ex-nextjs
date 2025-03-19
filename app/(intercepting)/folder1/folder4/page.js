98917477baa7f21be3ec77dcc45ab0600fa052d4
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-19 10:01:43 +0600

    create incepta of folder3

--- app/(intercepting)/folder1/folder4/page.js
+++ app/(intercepting)/folder1/folder4/page.js
@@ -4,7 +4,7 @@ export default function Folder4() {
     return (
         <div className="p-20 flex flex-col gap-10">
             <h1 className="text-2xl">Folder 4</h1>
-            <Link href="/folder4/folder3" className="text-blue-400">
+            <Link href="/folder1/folder3" className="text-blue-400">
                 Folder 3
             </Link>
             <Link href="/contact" className="text-green-400">
