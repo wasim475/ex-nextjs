258443dff37d9c0ff33fc56e97820852b04757bd
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-20 10:02:22 +0600

    Update page.js

--- app/(Combining Parallel & Intercepting Routes)/gallery/page.js
+++ app/(Combining Parallel & Intercepting Routes)/gallery/page.js
@@ -6,7 +6,7 @@ export default function Gallery() {
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-center text-3xl font-bold my-4">
-                Photos of a lost guy
+                Photos of Afrin Soha
             </h1>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                 {photos.map(({ id, src, name }) => (
