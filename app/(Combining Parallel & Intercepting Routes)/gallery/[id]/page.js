be61f83580b97c9c069b504d72033ec10190775b
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-20 09:47:30 +0600

    Update page.js

--- app/(Combining Parallel & Intercepting Routes)/gallery/[id]/page.js
+++ app/(Combining Parallel & Intercepting Routes)/gallery/[id]/page.js
@@ -7,7 +7,7 @@ export default function PhotoPage({ params }) {
 
     return (
         <div className="container mx-auto my-10">
-            <div className="w-1/2 mx-auto">
+            <div className="w-full mx-auto">
                 <div>
                     <h1 className="text-center text-3xl font-bold my-4">
                         {photo.name}
