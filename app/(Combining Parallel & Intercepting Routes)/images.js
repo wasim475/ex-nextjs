e4478c4bdb51aa97f1634d60131c705f299f66e1
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-20 09:39:59 +0600

    Update images.js

--- app/(Combining Parallel & Intercepting Routes)/images.js
+++ app/(Combining Parallel & Intercepting Routes)/images.js
@@ -9,37 +9,37 @@ import photo7 from "./photos/7.jpg";
 const images = [
     {
         id: "1",
-        name: "Sumit Saha - 1",
+        name: "Afrin Soha - 1",
         src: photo1,
     },
     {
         id: "2",
-        name: "Sumit Saha - 2",
+        name: "Afrin Soha - 2",
         src: photo2,
     },
     {
         id: "3",
-        name: "Sumit Saha - 3",
+        name: "Afrin Soha - 3",
         src: photo3,
     },
     {
         id: "4",
-        name: "Sumit Saha - 4",
+        name: "Afrin Soha - 4",
         src: photo4,
     },
     {
         id: "5",
-        name: "Sumit Saha - 5",
+        name: "Afrin Soha - 5",
         src: photo5,
     },
     {
         id: "6",
-        name: "Sumit Saha - 6",
+        name: "Afrin Soha - 6",
         src: photo6,
     },
     {
         id: "7",
-        name: "Sumit Saha - 7",
+        name: "Afrin Soha - 7",
         src: photo7,
     },
 ];
