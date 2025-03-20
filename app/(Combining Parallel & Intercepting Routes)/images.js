3d725e2270d90cf2d4cefe1667d07c9e431f341b
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-20 09:35:36 +0600

    Update images.js

--- app/(Combining Parallel & Intercepting Routes)/images.js
+++ app/(Combining Parallel & Intercepting Routes)/images.js
@@ -25,7 +25,7 @@ const images = [
     {
         id: "4",
         name: "Sumit Saha - 4",
-        src: photo4,
+        src: "https://scontent.fdac2-2.fna.fbcdn.net/v/t39.30808-6/472883461_1319392269499557_929381438153548185_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEJl0PVIZ6jC4XrtvxPE3PsvpASMqKPp0a-kBIyoo-nRm5oPCnMspDLz2MUuf2W6yLazwsRcj0ftgPmqvxyj-ey&_nc_ohc=2Er08COz5DwQ7kNvgFOBQy_&_nc_oc=Adm-yTNWApwpOkoD9MdBDW6JHeIs2U9LndDs0PsQUpU5IcmwMo3dz009rVcH6AfRcrFxzavaHTKTKNUrQzGTB36s&_nc_zt=23&_nc_ht=scontent.fdac2-2.fna&_nc_gid=o73i0Dcs0mJycO_1Mrls1g&oh=00_AYG-pa0OiHsMMQtpuIGAFt6qTQ7-eibzwggTQ_lF7N6eiQ&oe=67E14833",
     },
     {
         id: "5",
