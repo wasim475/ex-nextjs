1ba41a2617825634fd9b7a5e90e03c7527284045
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-20 09:38:25 +0600

    update on images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['scontent.fdac2-2.fna.fbcdn.net'],
+      },
+};
 
 export default nextConfig;
