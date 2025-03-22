aa9ae8bf6e70f6d120a26affab653f2571e957e5
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-22 09:12:41 +0600

    Update route.js

--- app/(Intro to Route Handlers - GET, POST, PATCH, DELETE)/api/comments/route.js
+++ app/(Intro to Route Handlers - GET, POST, PATCH, DELETE)/api/comments/route.js
@@ -6,5 +6,11 @@ export async function GET(){
 
 
 export async function POST(request){
-    
+    const comment = await request.json()
+    const newComment = {
+        id: comments.length +1,
+        text: comment.text
+    }
+    comments.push(newComment)
+    return Response.json(newComment)
 }
