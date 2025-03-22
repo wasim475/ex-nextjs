efcbf745855b59daabfa8215a4b81217ce18f007
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-22 09:56:46 +0600

    Update route.js

--- app/(Intro to Route Handlers - GET, POST, PATCH, DELETE)/api/comments/[id]/route.js
+++ app/(Intro to Route Handlers - GET, POST, PATCH, DELETE)/api/comments/[id]/route.js
@@ -1,6 +1,6 @@
 import comments from '../../../data/comments'
 
-export async function GET(request, {params}){
+export async function GET(_request, {params}){  //option parametre er jonn _ use kore hoy... optional ja kichur jonno eti use hoy...
     const commentId = params.id
     const comment = comments.find(comment=>comment.id === parseInt(commentId))
     return Response.json(comment)
