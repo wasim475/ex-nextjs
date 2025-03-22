7864facc98c827cf2877f17b22acdc9035f600e7
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-22 08:30:04 +0600

    Create comments.js

---
+++ app/(Intro to Route Handlers - GET, POST, PATCH, DELETE)/data/comments.js
@@ -0,0 +1,22 @@
+const comments =[
+    {
+        id:1,
+        text:"comment-1"
+    },
+    {
+        id:2,
+        text:"comment-2"
+    },
+    {
+        id:3,
+        text:"comment-3"
+    },
+    {
+        id:4,
+        text:"comment-4"
+    },
+    {
+        id:5,
+        text:"comment-5"
+    },
+]
