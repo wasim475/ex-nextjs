4d8e0722a5e62a262703228fbf4bcccea81c0e07
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-23 08:09:54 +0600

    Update route.js

--- app/(Intro to Route Handlers - GET, POST, PATCH, DELETE)/api/profile/route.js
+++ app/(Intro to Route Handlers - GET, POST, PATCH, DELETE)/api/profile/route.js
@@ -1,9 +1,9 @@
 import { headers } from 'next/headers';
 
 export async function GET(request){
-    const requestHeaders = new Headers(request.headers);
-    const headerList = headers()
-     console.log(requestHeaders.get("Authorization"));
+    // const requestHeaders = new Headers(request.headers); //row system to get headers
+    const headerList = headers() //nextjs to get headers
+     console.log(headerList.get("Authorization"));
 
      return new Response("profile-Info")
 }
