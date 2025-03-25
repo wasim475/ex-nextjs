63542336d795f2435bf73a0edc3d6f27f566e2af
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-25 09:29:55 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
 
-let count = 1
+
 export function middleware(request){
-    console.log("I'm inside in middleware.",count++)
+    console.log()
     return NextResponse.redirect(new URL('/gallery',request.url)) // request.url হলো ডোমেইন নেম লাইক- localhost:3000
     // return NextResponse.next()
 }
