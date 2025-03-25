30cc21d1083ea76fb0d51c18d5dd29b1b566826c
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-25 09:11:42 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,7 +2,5 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request){
     console.log("I'm inside in middleware.")
-    return NextResponse.json({
-        hello: "Hello World"
-    })
+    return NextResponse.next()
 }
