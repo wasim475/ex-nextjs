b605a4b087232dd02fc2a0a3b72eff2b11c49a53
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-25 10:39:35 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -71,3 +71,23 @@ export function middleware(request) {
   return response
 }
 */
+
+
+//waitUntil and NextFetchEvent
+
+/**
+ import { NextResponse } from 'next/server'
+import type { NextFetchEvent, NextRequest } from 'next/server'
+ 
+export function middleware(req: NextRequest, event: NextFetchEvent) {
+  event.waitUntil(
+    fetch('https://my-analytics-platform.com', {
+      method: 'POST',
+      body: JSON.stringify({ pathname: req.nextUrl.pathname }),
+    })
+  )
+ 
+  return NextResponse.next()
+}
+
+ */
