56bad4034b651a9d3b019664316d647e44af78be
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-25 12:49:00 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,7 @@
-let defaultLocale = ''
+let defaultLocale = 'en'
+let locales = ['bn','en']
+
+export function middleware(request){
+    const pathName = request.nextUrl.pathname
+    const pathNameIsMissingLocale = locales.every((locale)=>pathName.startsWith(`/${locale}`),pathName.startsWith(`/${locale}/`))
+}
