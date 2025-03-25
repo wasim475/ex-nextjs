e216876cbe96cb22644374f5e6af0c9de06b9d4d
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-25 10:16:16 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -46,3 +46,28 @@ export function middleware(request) {
   return response
 }
   */
+
+
+//Setting Headers
+
+/*
+import { NextResponse } from 'next/server'
+ 
+export function middleware(request) {
+  // Clone the request headers and set a new header `x-hello-from-middleware1`
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-hello-from-middleware1', 'hello')
+ 
+  // You can also set request headers in NextResponse.rewrite
+  const response = NextResponse.next({
+    request: {
+      // New request headers
+      headers: requestHeaders,
+    },
+  })
+ 
+  // Set a new response header `x-hello-from-middleware2`
+  response.headers.set('x-hello-from-middleware2', 'hello')
+  return response
+}
+*/
