d28ef952bf973c2d4b307d6c6d3de815aee3681d
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-25 10:53:26 +0600

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -1,9 +1,14 @@
+'use client'
+
+import { usePathname } from 'next/navigation';
+
 export default function NotFound() {
+    const pathName = usePathname()
     return (
         <div>
             <h1 className="font-bold text-xl">Page not found</h1>
             <div className="mt-5">
-                The page you are requesting was not found!
+                The page you are requesting({pathName}) was not found!
             </div>
         </div>
     );
