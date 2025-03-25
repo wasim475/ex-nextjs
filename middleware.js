f4537d2a767235a71f22c377f6a7277165dcc0e1
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-25 10:06:29 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,7 +4,8 @@ import { NextResponse } from 'next/server';
 export function middleware(request){
     // console.log(request.url)
     
-     return NextResponse.redirect(new URL('/gallery',request.url)) // request.url হলো ডোমেইন নেম লাইক- localhost:3000
+    //  return NextResponse.redirect(new URL('/gallery',request.url)) // request.url হলো ডোমেইন নেম লাইক- localhost:3000
+     return NextResponse.rewrite(new URL('/gallery',request.url)) // rewrite এর কাজ হলো, পাথনেম ঠিকই থাকবে কিন্তু কনটেন্ট দেখেবে gallery এর...
     
     // return NextResponse.next()
 }
