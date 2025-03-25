d5f6768b31d5158618a1a17d85615cb365f035af
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-25 09:21:53 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server';
 
+let count = 1
 export function middleware(request){
-    console.log("I'm inside in middleware.")
-    
+    console.log("I'm inside in middleware.",count++)
+    return NextResponse.redirect(new URL('/gallery',request.url))
     // return NextResponse.next()
 }
