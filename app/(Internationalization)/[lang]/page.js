4bb1c9c610ccfefeb117c29935ae172e41a5012f
Wasim Hossain <wasim.hossain003@gmail.com>
2025-03-27 09:20:13 +0600

    Update page.js

--- app/(Internationalization)/[lang]/page.js
+++ app/(Internationalization)/[lang]/page.js
@@ -1,7 +1,7 @@
-// import { getDictionary } from "./dictionaries/dictionaries";
+import { getDictionary } from "./dictionaries/dictionaries";
 
 export default async function Homepage({ params: { lang } }) {
-    // const dict = await getDictionary(lang);
+    const dict = await getDictionary(lang);
 
     return <div>{dict.hello}</div>;
 }
